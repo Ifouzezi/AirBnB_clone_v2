f867b9e8ea5028e29d9d459b5be849ccaa13ebc1
ifouzezi <ifoghaleuzezi@gmail.com>
2023-08-17 09:22:50 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """simple flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return render_template("100-hbnb.html")
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=None)

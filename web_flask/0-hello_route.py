ecd92e41a4ee0d9bf633d247ec7e923458e83050
ifouzezi <ifoghaleuzezi@gmail.com>
2023-08-17 08:00:25 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/airbnb-onepage/')
+def hello_airbnb():
+   return "Hello HBNB!"
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
